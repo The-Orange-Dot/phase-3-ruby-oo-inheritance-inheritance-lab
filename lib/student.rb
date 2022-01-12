9cf65e2a6e955c0e0127038f9af97162b5bcea27
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-12 15:26:39 +0900

    Completed

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+class Student < User
+
+  @first_name = "Steve"
+  @last_name = "Jobs"
+
+  def initialize
+  @knowledge = []
+  end
+    
+  def learn(learn)
+    @knowledge << learn
+  end
+
+  def knowledge
+    @knowledge
+  end
 
 end
